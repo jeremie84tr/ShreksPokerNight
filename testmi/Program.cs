b663cd9ee81408b32f389068a5597fabd2dc2a07
Jérémie <jeremie.lohyer@alumni.univ-avignon.fr>
2024-03-15 00:31:39 +0100

    testmi marche, reste à modif, test garder pour ref sur API

---
+++ testmi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text;
+
+namespace test
+{
+    public class test
+    {
+        private static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("https://jsonplaceholder.typicode.com")//https://jsonplaceholder.typicode.com
+        };
+        static async Task PostAsync(HttpClient httpClient)
+        {
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(new
+                {
+                    userId = 77,
+                    id = 1,
+                    title = "write code sample",
+                    completed = false
+                }),
+                Encoding.UTF8,
+                "application/json");
+
+            using HttpResponseMessage response = await httpClient.PostAsync(
+                "todos",
+                jsonContent);
+
+            response.EnsureSuccessStatusCode();
+                /*.WriteRequestToConsole();*/
+            
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+
+            // Expected output:
+            //   POST https://jsonplaceholder.typicode.com/todos HTTP/1.1
+            //   {
+            //     "userId": 77,
+            //     "id": 201,
+            //     "title": "write code sample",
+            //     "completed": false
+            //   }
+        }
+        public static async Task Main()
+        {
+            await PostAsync(sharedClient);
+        }
+    }
+}
